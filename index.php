01ff306ffc407f74ced6ecd3ee2d1bb58fcd7598
David Wosnitza <david@druul.in>
2013-11-10 09:02:36 +0100

    switched path vars to constants

--- index.php
+++ index.php
@@ -1,57 +1,62 @@
 <?php
 // Because why the fuck not
 interface iHook {
-	public static function run($args, $mail, $options = array());
+	public static function run($args, $mail, define('OPTIONS', array()));
 }
 
-$basepath = dirname(__FILE__) . DIRECTORY_SEPARATOR;
-$hookpath = $basepath . 'hook' . DIRECTORY_SEPARATOR;
-$confpath = $basepath . 'conf' . DIRECTORY_SEPARATOR;
+// Define essential paths
+define('BASEPATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+define('HOOKPATH', BASEPATH . 'hook' . DIRECTORY_SEPARATOR);
+define('CONFPATH', BASEPATH . 'conf' . DIRECTORY_SEPARATOR);
+define('DEPSPATH', BASEPATH . 'deps' . DIRECTORY_SEPARATOR);
 
 // Get users and make sure the "error" user is declared
-$users = json_decode(file_get_contents($confpath . 'users.json'));
-if (!$users || !$users->{'!error'}->mail || !file_exists($hookpath . 'Err.php')) { die(); };
-$errormail = $users->{'!error'}->mail ;
+define('USERS', json_decode(file_get_contents(CONFPATH . 'users.json')));
+if (!$users || !$users->{'!error'}->mail || !file_exists(HOOKPATH . 'Err.php')) { die(); };
+define('ERRORMAIL', $users->{'!error'}->mail );
 
 // Extract payload or die
-$payload = json_decode(str_replace("\n", '\n', stripslashes($_POST['payload'])));
+define('PAYLOAD', json_decode(str_replace("\n", '\n', stripslashes($_POST['payload']))));
 if (!(
 	is_object($payload)
 	&& (property_exists($payload, 'commits') || property_exists($payload, 'head_commit') )
 	&& is_array($payload->commits)
 )) { die(); }
-$commits = empty($payload->commits) ? array($payload->head_commit) : $payload->commits;
+define('COMMITS', empty($payload->commits) ? array($payload->head_commit) : $payload->commits);
 
 foreach ($commits as $commit) {
 	try {
 
 		list($exec, $hook, $args) =  @explode(' ', @$commit->message, 3);
 		if(strtoupper($exec) !== 'EXEC') { continue; }
-		$hook = ucfirst(strtolower($hook));
-		$args = trim($args);
+		define('HOOK', ucfirst(strtolower($hook)));
+		define('ARGS', trim($args));
 
-		$user = $commit->committer->username;
-		$mail = $users->$user->mail;
-		$opts = $users->$user->options;
+		define('USER', $commit->committer->username);
+		define('MAIL', $users->$user->mail);
+		define('OPTS', $users->$user->options);
 
 		if (!$user || !$mail) {
 			throw new Exception(sprintf('Invalid user: %s, or email: %s', $user, $mail));
 		}
 
-		if (!$hook || !ctype_alpha($hook) || !file_exists($hookpath . $hook . '.php')) {
+		if (!$hook || !ctype_alpha($hook) || !file_exists(HOOKPATH . $hook . '.php')) {
 			throw new Exception(sprintf('Invalid hook: %s', $hook));
 		}
 
-		require_once($hookpath . $hook . '.php');
+		require_once(HOOKPATH . $hook . '.php');
 		if (!class_exists($hook)) {
 			throw new Exception('Hook file found, but class not declared: %s', $hook);
 		}
 
-		call_user_func_array(array($hook, 'run'), array($args, $mail, $opts));
+		define('OUTPUT', call_user_func_array(array($hook, 'run'), array($args, $mail, $opts)));
+
+		require_once(HOOKPATH . 'Result.php');
+		Result::run($args, $mail, $opts);
 
 	} catch (Exception $e) {
-		$args = $e->getMessage() . "\n\nOriginal args: " . $args;
-		require_once($hookpath . 'Err.php');
+		define('ARGS', $e->getMessage() . "\n\nOriginal args: " . $args);
+		require_once(HOOKPATH . 'Err.php');
 		Err::run($args, $errormail);
 	}
 }
