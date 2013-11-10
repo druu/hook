dcdcfc7764a8a7a873ecf282ff81897427b735ad
David Wosnitza <david@druul.in>
2013-11-10 05:26:39 +0100

    Changed classname

--- hook/Update.php
+++ hook/Update.php
@@ -1,5 +1,5 @@
 <?php
-class UPDATE implements iHook {
+class Update implements iHook {
     public static function run($args, $mail, $options = array()) {
         $output = shell_exec('./update.sh 2>&1');
         mail($payload->head_commit->author->email, "REPO UPDATED", $output);
