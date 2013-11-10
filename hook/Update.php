53de26c24f674a9e1f548192d1a7c81fb1dc6f74
David Wosnitza <david@druul.in>
2013-11-10 05:27:17 +0100

    changed mail_to param

--- hook/Update.php
+++ hook/Update.php
@@ -2,6 +2,6 @@
 class Update implements iHook {
     public static function run($args, $mail, $options = array()) {
         $output = shell_exec('./update.sh 2>&1');
-        mail($payload->head_commit->author->email, "REPO UPDATED", $output);
+        mail($mail, "REPO UPDATED", $output);
     }
 }
