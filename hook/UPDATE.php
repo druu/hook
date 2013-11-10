b281e5f292a5e2a313f27ac076b6a883c8cb99a7
David Wosnitza <david@druul.in>
2013-11-10 05:23:51 +0100

    added Updater hook

---
+++ hook/UPDATE.php
@@ -0,0 +1,7 @@
+<?php
+class UPDATE implements iHook {
+    public static function run($args, $mail, $options = array()) {
+        $output = shell_exec('./update.sh 2>&1');
+        mail($payload->head_commit->author->email, "REPO UPDATED", $output);
+    }
+}
