bdaedb5ffdb83e89c76db9ba1b72b52e01fe0311
David Wosnitza <david@druul.in>
2013-11-10 09:26:42 +0100

    modified Update hook to use Return hook

--- hook/Update.php
+++ hook/Update.php
@@ -1,7 +1,9 @@
 <?php
 class Update implements iHook {
-    public static function run($args, $mail, $options = array()) {
+    public static function run($args, $mail, stdClass $options) {
         $output = shell_exec('./update.sh 2>&1');
-        mail($mail, "REPO UPDATED", $output);
+        return isset($options->type) && $options->type === 'html'
+            ? '<pre>' . $output . '</pre>'
+            : $output;
     }
 }
